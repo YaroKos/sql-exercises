7145390ba673f56883a860d0205b05fbaf21c181
Yaro <yaro@localhost.localdomain>
2018-09-03 16:56:42 +0200

    module 1, lab 1

---
+++ Module_1/Lab_1-1 - Retrieving Customer Data.sql
@@ -0,0 +1,3 @@
+-- select all columns
+-- from the SalesLT.Customer table
+SELECT * FROM SalesLT.Customer
---
+++ Module_1/Lab_1-3 - Create List of Customer Contacts (2).sql
@@ -0,0 +1,3 @@
+-- finish the query
+SELECT SalesPerson, Title + ' ' + LastName AS CustomerName, Phone
+FROM SalesLT.Customer;
